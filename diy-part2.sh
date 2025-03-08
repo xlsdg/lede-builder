956683e22120e9247d9b3e2be28db622f234b803
Zhentian Lu <xlsdg@qq.com>
2025-03-08 14:20:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,13 +11,15 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.88.9/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.88.9/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.88.9/g' package/base-files/luci2/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/Lede/g' package/base-files/luci2/bin/config_generate
 
 mkdir -p files/etc/openclash/core
 
