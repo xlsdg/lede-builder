f9a404edc79f4972c50977b14a78ccf9b2c4c8b0
Zhentian Lu <xlsdg@qq.com>
2025-03-19 01:07:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,8 @@
 # See /LICENSE for more information.
 #
 
+rm -rf feeds/luci/applications/luci-app-openclash
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.88.9/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.88.9/g' package/base-files/luci2/bin/config_generate
